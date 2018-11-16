4e8452a51156f9b380b7ab772cc4b7be86d51cf0
Christopher <stock.c@cambridgeenglish.org>
2018-11-16 11:18:22 +0000

    added code comments

--- src/javascripts/modules/app.js
+++ src/javascripts/modules/app.js
@@ -8,7 +8,9 @@ class App {
   }
 
   /**
-   * Initialize module
+   * Retrieve setting value
+   * @param {String} label the name of the setting we need the value for
+   * @return {Promise} resolves to array of values for given setting
    */
   async settings(label) {
     let setting_values = (await this._client.metadata()).settings[label];
@@ -16,7 +18,10 @@ class App {
   };
 
   /**
-   * Initialize module
+   * Check if values (for a user) exist in a collection (of setting values)
+   * @param {Array} collection array of strings containing setting values
+   * @param {Array || String} values user data values relating to id, tags, group or org as array 
+   * @returns {Boolean} 
    */
   contains(collection, values) {
     if (typeof values !== "object")
@@ -30,7 +35,9 @@ class App {
   };
 
   /**
-   * Initialize module
+   * Establish if logged in user is affected by Assignment Control settings
+   * @param {Object} user Zendesk user Object
+   * @returns {Promise} resolves to Boolean
    */
   async currentUserIsTarget(user) {
     const rules = [
@@ -44,7 +51,7 @@ class App {
       })]
     ];
 
-    const pArray = rules.map(n => this.funcOne(n));
+    const pArray = rules.map(n => this.userMatchesRule(n));
     const results = await Promise.all(pArray);
     const isTarget = results.some((res) => {
       return res === true;
@@ -53,15 +60,18 @@ class App {
   };
 
   /**
-   * Initialize module
+   * Test if a rule applies to a user
+   * @param {Array} rule array of arrays containing rule names and user data
+   * @returns {Promise} resolves to Boolean
    */
-  async funcOne(rule) {
+  async userMatchesRule(rule) {
     const setting_values = await this.settings(rule[0]);
     return this.contains(setting_values, rule[1]);
   }
 
   /**
-   * Initialize module
+   * Hides assignee options
+   * @returns nothing
    */
   async hideAssigneeOptions() {
     const group_ids = await this.settings('hidden_group_ids');
@@ -69,6 +79,7 @@ class App {
 
     const assigneeOptions = (await this._client.get('ticketFields:assignee'))['ticketFields:assignee'];
 
+    // hide assignee groups
     assigneeOptions.optionGroups.forEach((option, index) => {
       const group_id = option.value;
 
@@ -77,6 +88,7 @@ class App {
       }
     });
 
+    // hide assignee values (i.e. agents)
     assigneeOptions.optionValues.forEach((option, index) => {
       const group_and_user = option.value.split(':'),
         user_id = group_and_user[1] || "";
